5f17d2d33521133bd3b9688719f16f6fd235b6e1
nevin <nevinsebastian@cse.carmelcet.in>
2023-10-15 23:41:43 +0530

    ipsweeper

--- week2/python101.py
+++ week2/python101.py
@@ -174,7 +174,120 @@ print("Tuples have parentheses and cannot change")
 grades = ("A","B","C","D","E","F")
 print(grades[1])
 
+    Get Kali
+    Blog
+    Documentation
+    Kali Linux Documentation
+    Kali Tools Documentation
+    Known Issues
+    Community
+    Community Support
+    Kali Linux Forums
+    Discord
+    Join Newsletter
+    Mirror Location
+    Get Involved
+    Courses
+    Kali Linux Revealed (KLCP/PEN-103)
+    PEN-200 (PWK/OSCP)
+    PEN-210 (WiFu/OSWP)
+    PEN-300 (ETBD/OSEP)
+    WEB-200 (WAWK/OSWA)
+    WEB-300 (AWAE/OSWE)
+    EXP-301 (WUMED/OSED)
+    EXP-312 (MCB/OSMR)
+    EXP-401 (AWE/OSEE)
+    SOC-200 (OSDA)
+    Developers
+    Git Repositories
+    Packages
+    Auto Package Test
+    Bug Tracker
+    Kali NetHunter Stats
+    About
+    Kali Linux Overview
+    Press Pack
+    Meet The Kali Team
+    Partnerships
+    Contact Us
 
+    Tools
+        Installing Flatpak on Kali Linux
+        Installing snapd on Kali Linux
+        Installing Tor Browser on Kali Linux
+        Kali Tools
+        Metasploit Framework
+        Removed Tools From Kali
+        Submitting tools to Kali
+
+LIGHT
+DARK
+Kali Tools
+Table of Contents
+
+Users have a few areas where they can look for information about a tool in Kali.
+Local information
+
+The first place users should look to is whatever local information is available. Through man pages and help output, many uses of tools will be seen. Remember to just run man tool-name or tool-name --help when you’re unsure of a certain flag and you should be good. However, what if you don’t know at all what a tool is used for, and you don’t learn much from either resource?
+Online information
+
+If man pages and help output didn’t answer questions, then perhaps tools.kali will be of assistance. With use case examples, the man page/help output, and descriptions for the tool, there is a lot of information to be gained by looking at a tool. Additionally, each tool’s page links to the upstream website which may provide additional information for the tool.
+
+Updated on: 2023-Mar-13
+Author: gamb1t
+Edit this page
+Create a new page
+Installing Tor Browser on Kali Linux
+Metasploit Framework
+LIGHT
+DARK
+Links
+Home
+Download / Get Kali
+Blog
+OS Documentation
+Tool Documentation
+System Status
+Archived Releases
+Platforms
+ARM (SBC)
+NetHunter (Mobile)
+
+Amazon AWS
+Docker
+Linode
+Microsoft Azure
+Microsoft Store (WSL)
+Vagrant
+Development
+Bug Tracker
+Continuous Integration
+Network Mirror
+Package Tracker
+GitLab
+Community
+Discord
+Support Forum
+Follow Us
+Facebook
+Instagram
+Mastodon
+Twitter
+
+Newsletter
+RSS
+Policies
+Cookie Policy
+Privacy Policy
+Trademark Policy
+© OffSec Services Limited 2023. All rights reserved.
 #Loopinf
 print("For loops - start to finish of iterate:")
 vegitables = ["cucumber", "spinach","cabbage"
+
+
+
+
+,skngslfkgjegjhzepgz;dhnd;rhne;hner;hndf;flhfxlhnrHn
+
+shdnfjx
