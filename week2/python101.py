2f0854809baf34f1a75201471a9d22e6631bca0b
nevin <nevinsebastian@cse.carmelcet.in>
2023-10-15 10:19:23 +0530

    python

---
+++ week2/python101.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+print("strings and things:")
+print("""hello this is
+a muliti line string""")
+
+print ("this is "+"a string")
+
+print('\n') #nev line
+
+print("math time:")
+print(50 + 50)
