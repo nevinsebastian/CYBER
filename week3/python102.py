95502dd07e8cd65284c1f04c30a50674364dfc65
nevin <nevinsebastian@cse.carmelcet.in>
2023-10-18 04:20:17 +0530

    week3

--- week3/python102.py
+++ week3/python102.py
@@ -1,1 +1,52 @@
-dd
+#!/bin/python3
+
+#Importing
+print("Importing is important")
+
+
+import sys #system functions and parameters
+
+from datetime import datetime
+print(datetime.now())
+
+from datetime import datetime as dt #importing with an alias
+print(dt.now())
+
+def new_line():
+	print('\n')
+	
+new_line()
+
+#Advanced Strings
+print("Advanced strings:")
+my_name = "Nevin"
+print(my_name[0])
+print(my_name[-1])
+
+sentence = "this is a sentence"
+print(sentence[:4]) # to print the first word
+print(sentence[-9:-1])
+
+print(sentence.split())
+
+sentence_split = sentence.split()
+sentence_join = ' '.join(sentence_split)
+print(sentence_join)
+print('\n'.join(sentence_split))
+
+
+
+qouteception = "I sed, 'give me all the money"
+print(qouteception)
+
+qouteception = "I sed, \"give me all the money\""
+print(qouteception)
+
+print("A" in "Apple")
+letter = "a"
+word= "Apple"
+print(letter.lower() in word.lower())# Improved case insensitive
+
+word_two = "Bingo"
+print((letter.lower() in word.lower()) and not (letter.lower() in word_two.lower()))
+
